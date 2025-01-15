44730a6c01c2781b97f48520e436dafa35a4b6c0
nurealamriyal <nurealamriyal@gmail.com>
2025-01-15 13:04:48 +0600

    added property

--- index.js
+++ index.js
@@ -26,6 +26,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection=client.db('real-State-management').collection('user')
+    const propertyCollection=client.db('real-State-management').collection('property')
+   
+    app.get('/user/:email',async (req,res) => {
+         const email=req.params.email
+         const query={email}
+         const result=await userCollection.findOne(query)
+         res.send(result)
+    })
+    app.get('/alluser',async (req,res) => {
+    
+         const query={role:{
+          $ne:"Admin"
+         }}
+         const result=await userCollection.find(query).toArray()
+         res.send(result)
+    })
+    app.post('/user',async (req,res) => {
+      const user=req.body
+      const query={email:user?.email}
+      const isExist=await userCollection.findOne(query)
+      if(isExist)
+          return 
+      const result= await userCollection.insertOne(user)
+      res.send(result)
+  })
+    app.post('/property',async (req,res) => {
+      const property=req.body
+      const result=await propertyCollection.insertOne(property)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -40,6 +71,7 @@ run().catch(console.dir);
 app.get('/',(req,res)=>{
     res.send('Serrver in running')
 })
+
 app.listen(port,()=>{
     console.log(`server in running on the Port is ${port}`)
 })
